b654c19c21d546ff252d609c639f19c5f7f1f803
Kirill Zhdanov <storenth@yandex.ru>
2024-02-25 18:57:31 +0700

    fix: test: remove duplicated output

--- src/tests/core_test.py
+++ src/tests/core_test.py
@@ -17,12 +17,22 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from pnk import core
 
 
-TestData = namedtuple('TestData', ['increment', 'cartesian', 'data', 'wordlist'])
+TestData = namedtuple(
+    'TestData',
+    [
+        'increment',
+        'cartesian',
+        'data',
+        'replace',
+        'target',
+        'wordlist',
+    ]
+)
 
 class Permutation(unittest.TestCase):
     def setUp(self):
         # no --increment or --wordlist option given
-        self.args = TestData("", "", "", "")
+        self.args = TestData("", "", "", "", "", "")
         self.original_stdout, sys.stdout = sys.stdout, StringIO()
 
     def test_two_permutation(self):
@@ -32,8 +42,6 @@ class Permutation(unittest.TestCase):
             sys.stdout.getvalue(),
             (
                 "1a-2b3c.4tutu.google.com\n"
-                "1a_2b3c.4tutu.google.com\n"
-                "1a.2b3c.4tutu.google.com\n"
                 "4tutu.1a-2b3c.google.com\n"
             ),
         )
@@ -45,7 +53,7 @@ class Permutation(unittest.TestCase):
 class Incrementation(unittest.TestCase):
     def setUp(self):
         # --increment option enabled
-        self.args = TestData("i", "", "", "")
+        self.args = TestData("i", "", "", "", "", "")
         self.original_stdout, sys.stdout = sys.stdout, StringIO()
 
     def test_one_sub_increment(self):
@@ -72,6 +80,29 @@ class Incrementation(unittest.TestCase):
 
     def tearDown(self):
         sys.stdout = self.original_stdout
+
+class Replace(unittest.TestCase):
+    def setUp(self):
+        # --replace option given
+        self.args = TestData("", "", "", "r", "", "")
+        self.original_stdout, sys.stdout = sys.stdout, StringIO()
+
+    def test_two_permutation(self):
+        with open(pathlib.Path(__file__).parent / 'one.txt', 'r', encoding='UTF-8') as file:
+            core.Formula(self.args, [file]).run()
+        self.assertEqual(
+            sys.stdout.getvalue(),
+            (
+                "1a-2b3c.4tutu.google.com\n"
+                "1a_2b3c.4tutu.google.com\n"
+                "1a.2b3c.4tutu.google.com\n"
+                "4tutu.1a-2b3c.google.com\n"
+            ),
+        )
+
+    def tearDown(self):
+        sys.stdout = self.original_stdout
+
 
 if __name__ == '__main__':
     unittest.main()
